dbdc0860ead3c0cccb85f8b12f8a631155f0623b
Konstantinos <ko.trian@gmail.com>
2018-05-12 16:46:55 +0200

    Remove Vector2D equals and introduce contentEquals

--- math/src/main/java/math/vectors/Vector2D.java
+++ math/src/main/java/math/vectors/Vector2D.java
@@ -5,22 +5,11 @@ public class Vector2D {
     public float y;
 
     public Vector2D(float pX, float pY) {
-        // TODO: initialize fields
+        x = pX;
+        y = pY;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Vector2D vector2D = (Vector2D) o;
-
-        // TODO: implement actual comparison
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        // TODO: implement hashcode
-        return 0;
+    public boolean contentEquals(Vector2D vec) {
+        return vec != null && x == vec.x && y == vec.y;
     }
 }
--- math/src/test/java/Vector2DTest.java
+++ math/src/test/java/Vector2DTest.java
@@ -1,52 +1,90 @@
 import math.vectors.Vector2D;
 
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.Assume;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
+import static org.hamcrest.CoreMatchers.*;
+
+
+@RunWith(Theories.class)
 public class Vector2DTest {
+    private static final int REPETITIONS = 5;
 
-    @Test
-    public void testCreation() {
-        Vector2D vec;
+    @DataPoints
+    public static float coordinates[] = {0.0f, -93.643f, 0.999999f, Float.MAX_VALUE, Float.MIN_VALUE,
+            Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY};
 
-        vec = new Vector2D(0, 0);
-        Assert.assertEquals(vec.x, 0, 0);
-        Assert.assertEquals(vec.y, 0, 0);
-        vec = new Vector2D(5.5f, 0);
-        Assert.assertEquals(vec.x, 5.5f, 0);
-        Assert.assertEquals(vec.y, 0, 0);
-        vec = new Vector2D(5.5f, 5.5f);
-        Assert.assertEquals(vec.x, 5.5f, 0);
-        Assert.assertEquals(vec.y, 5.5f, 0);
-        vec = new Vector2D(0, -5.5f);
-        Assert.assertEquals(vec.x, 0, 0);
-        Assert.assertEquals(vec.y, -5.5f, 0);
-        vec = new Vector2D(5.511f, -5.5f);
-        Assert.assertEquals(vec.x, 5.511f, 0);
-        Assert.assertEquals(vec.y, -5.5f, 0);
-        vec = new Vector2D(Float.MAX_VALUE, 0);
-        Assert.assertEquals(vec.x, Float.MAX_VALUE, 0);
-        Assert.assertEquals(vec.y, 0, 0);
-        vec = new Vector2D(5.789f, Float.MIN_VALUE);
-        Assert.assertEquals(vec.x, 5.789f, 0);
-        Assert.assertEquals(vec.y, Float.MIN_VALUE, 0);
-        vec = new Vector2D(Float.MIN_VALUE, Float.MAX_VALUE);
-        Assert.assertEquals(vec.x, Float.MIN_VALUE, 0);
-        Assert.assertEquals(vec.y, Float.MAX_VALUE, 0);
+    @Theory
+    public void testCreation(float x, float y) {
+        Vector2D vec = new Vector2D(x, y);
+        Assert.assertEquals(vec.x, x, 0);
+        Assert.assertEquals(vec.y, y, 0);
     }
 
-    /**
-     * Tests the equals method using the Assert.assertEquals method.
-     */
-    @Test
-    public void testEquality() {
-        Assert.assertEquals(new Vector2D(0,0), new Vector2D(0,0));
-        Assert.assertEquals(new Vector2D(55.5678f,0), new Vector2D(55.5678f,0));
-        Assert.assertEquals(new Vector2D(800f,-55.545678f), new Vector2D(800f,-55.545678f));
-        Assert.assertEquals(new Vector2D(0,Float.MAX_VALUE), new Vector2D(0, Float.MAX_VALUE));
-        Assert.assertEquals(new Vector2D(-0.9999f, Float.MIN_VALUE), new Vector2D(-0.9999f, Float.MIN_VALUE));
-        Assert.assertEquals(new Vector2D(Float.MAX_VALUE, Float.MIN_VALUE), new Vector2D(Float.MAX_VALUE, Float.MIN_VALUE));
-        Assert.assertEquals(new Vector2D(-1.2345678f, Float.POSITIVE_INFINITY), new Vector2D(-1.2345678f, Float.POSITIVE_INFINITY));
-        Assert.assertEquals(new Vector2D(Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY), new Vector2D(Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY));
+    @Theory
+    public void testContentEqualsIsReflexive(float x, float y) {
+        Vector2D vec = new Vector2D(x, y);
+        Assert.assertThat(vec.contentEquals(vec), is(true));
+    }
+
+    @Theory
+    public void testContentEqualsReturnsTrue(float x1, float y1, float x2, float y2) {
+        Vector2D vec1 = new Vector2D(x1, y1);
+        Vector2D vec2 = new Vector2D(x2, y2);
+        Assume.assumeTrue(x1 == x2 && y1 == y2);
+        Assert.assertThat(vec1.contentEquals(vec2), is(true));
+    }
+
+    @Theory
+    public void testContentEqualsReturnsFalse(float x1, float y1, float x2, float y2) {
+        Vector2D vec1 = new Vector2D(x1, y1);
+        Vector2D vec2 = new Vector2D(x2, y2);
+        Assume.assumeTrue(x1 != x2 || y1 != y2);
+        Assert.assertThat(vec1.contentEquals(vec2), is(false));
+    }
+
+    @Theory
+    public void testContentEqualsIsSymmetric(float x1, float y1, float x2, float y2) {
+        Vector2D vec1 = new Vector2D(x1, y1);
+        Vector2D vec2 = new Vector2D(x2, y2);
+        Assume.assumeThat(vec1.contentEquals(vec2), is(true));
+        Assert.assertThat(vec2.contentEquals(vec1), is(true));
+    }
+
+    @Theory
+    public void testContentEqualsIsTransitive(float x1, float y1, float x2, float y2, float x3, float y3) {
+        Vector2D vec1 = new Vector2D(x1, y1);
+        Vector2D vec2 = new Vector2D(x2, y2);
+        Vector2D vec3 = new Vector2D(x3, y3);
+        Assume.assumeThat(vec1.contentEquals(vec2), is(true));
+        Assume.assumeThat(vec2.contentEquals(vec3), is(true));
+        Assert.assertThat(vec1.contentEquals(vec3), is(true));
+    }
+
+    @Theory
+    public void testContentEqualsWithNull(float x, float y) {
+        Vector2D vec = new Vector2D(x, y);
+        Assert.assertThat(vec.contentEquals(null), is(false));
+    }
+
+    @Theory
+    public void testContentEqualsIsConsistent() {
+        Vector2D vec1 = new Vector2D(coordinates[1], coordinates[3]);
+        Vector2D vec2 = new Vector2D(coordinates[1], coordinates[3]);
+        Vector2D vec3 = new Vector2D(coordinates[1], coordinates[4]);
+        boolean[] resultsTrue = new boolean[REPETITIONS];
+        boolean[] resultsFalse = new boolean[REPETITIONS];
+
+        for (int run = 0; run < REPETITIONS; run++) {
+            resultsTrue[run] = vec1.contentEquals(vec2);
+            resultsFalse[run] = vec1.contentEquals(vec3);
+        }
+
+        Assert.assertArrayEquals(resultsTrue, new boolean[]{true, true, true, true, true});
+        Assert.assertArrayEquals(resultsFalse, new boolean[]{false, false, false, false, false});
     }
 }
